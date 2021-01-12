4050c72ea193bc8e31d20ff67154a3c01a491fdf
Dominique NORMAND <dominique@normand.us>
2021-01-12 09:11:13 -0500

    Simplified ( merged 2 methods )

--- src/main/scala/day11.scala
+++ src/main/scala/day11.scala
@@ -37,38 +37,32 @@ case class Day11()
 
   def isValid(password: String) : Boolean = rule1(password) && rule2(password) && rule3(password)
 
-  def nextPassword2(password: String) : String = {
+  def nextPassword(password: String) : String = {
     val (_, pwd) = password.foldRight((1, "")) {
+      case (c, (offset, pwd)) if (c == 'i') => (0, 'j'+ "a"*pwd.length)
+      case (c, (offset, pwd)) if (c == 'o') => (0, 'p'+ "a"*pwd.length)
+      case (c, (offset, pwd)) if (c == 'l') => (0, 'm'+ "a"*pwd.length)
+
       case (c, (offset, pwd)) if (c == 'z' && offset == 1) => (1, 'a' + pwd)
+
       case (c, (offset, pwd)) if (offset == 1 && c == 'h') => (0, 'j'+pwd) // skip i
       case (c, (offset, pwd)) if (offset == 1 && c == 'k') => (0, 'm'+pwd) // skip l
       case (c, (offset, pwd)) if (offset == 1 && c == 'n') => (0, 'p'+pwd) // skip o
+
       case (c, (offset, pwd)) if (offset == 1) => (0, nextChar(c)+pwd)
+
       case (c, (_, pwd)) => (0, c + pwd) 
     }
 
     pwd
   }
 
-  def nextPassword(password: String) : String = {
-    password indexOf 'i'  match {
-      case v if v >= 0 => password.substring(0, v-1) + 'j' + "a"*(password.length-v)
-      case _ => password indexOf 'o' match {
-        case v if v >= 0 => password.substring(0, v-1) + 'p' + "a"*(password.length-v)
-        case _ => password indexOf 'l' match {
-          case v if v >= 0 => password.substring(0, v-1) + 'm' + "a"*(password.length-v)
-          case _ => nextPassword2(password)
-        }
-      }
-    }
-  } 
-
   def nextValidPassword(pwd: String) : String =
   {
     var password = nextPassword(pwd)
 
     while (!isValid(password))
-      password = nextPassword2(password)
+      password = nextPassword(password)
 
     password
   }
@@ -80,6 +74,7 @@ case class Day11()
   def execute(): Unit = {
     println();
     println("--- Day 11 ---")
+    println(nextPassword("aiboclab"))
     val password = part1
     println(s"Answer to day 11 part 1 is $password")
     println(s"Answer to day 11 part 2 is ${part2(password)}")
