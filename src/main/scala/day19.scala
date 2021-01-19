183bf172e23729715047d8b9536e719f2563e742
Dominique NORMAND <dominique@normand.us>
2021-01-19 00:03:22 -0500

    day 19 and 20 done 20 not very scala like

--- src/main/scala/day19.scala
+++ src/main/scala/day19.scala
@@ -4,11 +4,73 @@ import scala.io.Source
 
 case class Day19()
 {
-  def getInput(): Iterator[String] = Source.fromResource("day19.data").getLines
+  case class Factory(items: Map[String, String], start: String)
+  {
+    private def search(input: String, to: String, from: String) : Iterable[String] = 
+    {
+      var data = Array[String]()
+      var idx = input.indexOf(from)
 
-  def part1(): Int = 0
+      while(idx >= 0) {
+        data = data :+ input.substring(0, idx) + to + input.substring(idx + from.length)
+        idx = input.indexOf(from, idx+1)
+      }  
 
-  def part2(): Int = 0
+      data.distinct
+    }
+
+    def calibrate() : Int =
+    {
+      var set = Set[String]()
+
+      items foreach { case (to, from) => set = set ++ search(start, to, from) }
+
+      return set.size
+    }
+
+    private def decrypt(molecules: Array[String]) : Array[String] =
+    {  
+      var set = Set[String]()
+      molecules.foreach(molecule => items foreach { case (to, from) => set = set ++ search(molecule, from, to) })
+      
+      val newMolecules = set.toArray.sortWith((s1, s2) => s1.length < s2.length).take(25);
+
+      newMolecules
+    }
+
+    def countSteps() : Int = 
+    {
+      var steps = 0;
+      var molecules = Array(start)
+
+      while (molecules.length > 0 && !(molecules contains "e"))
+      {
+        steps += 1
+        molecules = decrypt(molecules)
+      }
+
+      return steps
+    }
+  }
+
+  val parser = "(.*) => (.*)".r
+  def getInput(): Factory = {
+    val m = Source.fromResource("day19.data")
+            .getLines
+            .filter(line => line.length != 0)
+            .map({
+              case parser(from, to) 
+                => to -> from
+              case v 
+                => "*" -> v
+            })
+            .toMap
+    Factory(m - "*", m("*"))
+  }
+
+  def part1(): Int = getInput.calibrate
+
+  def part2(): Int = getInput.countSteps
 
   def execute(): Unit = {
     println();
--- src/main/scala/day20.scala
+++ src/main/scala/day20.scala
@@ -1,14 +1,78 @@
 package Days
 
 import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
 
 case class Day20()
 {
-  def getInput(): Iterator[String] = Source.fromResource("day20.data").getLines
+  def getInput(): Int = Source.fromResource("day20.data").getLines.toArray.head.toInt
 
-  def part1(): Int = 0
+  def divisors(n: Int): Array[Int] = {
+    var divs = if (n == 1) Array(1) else Array(1, n)
+    var max  = n 
+    var min  = 2
 
-  def part2(): Int = 0
+    while (min < max) 
+    {
+      if ((n % min) == 0) {
+        max = n / min
+        divs = divs :+ min
+        if (max > min) divs = divs :+ max
+      }
+      min += 1
+    }
+
+    divs.sortWith((a, b) => a < b);
+  }
+
+  def getHouseGiftCount(house: Int): Int =
+  {
+    var gifts = 0
+    for(elf <- divisors(house)) {
+      gifts += elf * 10
+    }
+    gifts
+  }
+
+  def part1(): Int = 
+  {
+    val maxGifts = getInput
+    var minHouse = maxGifts / 10 // to avoid using too much memory
+    val houses = ArrayBuffer.fill(minHouse+1) { 10 }
+
+    for(elf <- (2 until minHouse)) {
+      var droppedGifts = elf * 10
+      for(house <- Range(elf, minHouse, elf)) {
+        val gifts = houses(house) + droppedGifts
+        houses(house) = gifts
+        if (gifts >= maxGifts)
+          minHouse = house
+      }
+    }
+
+    minHouse
+  }
+
+  def part2(): Int =
+  {
+    val maxGifts = getInput
+    var minHouse = maxGifts / 10 // to avoid using too much memory
+    val houses   = ArrayBuffer.fill(minHouse+1) { 11 }
+
+    for(elf <- (2 until minHouse)) 
+    {
+      var droppedGifts = elf * 11
+      for(house <- Range(elf, scala.math.min(minHouse, 50 * elf), elf))
+      {
+        val gifts = houses(house) + droppedGifts
+        houses(house) = gifts
+        if (gifts >= maxGifts)
+          minHouse = house
+      }
+    }
+
+    minHouse
+  }
 
   def execute(): Unit = {
     println();
